3a111e9cb75187aa7f3ac24c7b78ceee398459ba
Rolf-Erik Christensen <chrrol12@nith.no>
2013-04-12 19:53:30 +0200

    Added comments and cleaned the code
    Animation.cs

--- Shooter/Shooter/Shooter/Animation.cs
+++ Shooter/Shooter/Shooter/Animation.cs
@@ -17,22 +17,48 @@ namespace Shooter
         public int CurrentFrame = 0;
         public event EventHandler OnFinished;
 
+        private float timer = 0;
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
         public Animation()
         {
         }
+
+        /// <summary>
+        /// Value constructor which receives a value and adds it to the Rectangles range
+        /// </summary>
+        /// <param name="Rectangles"></param>
         public Animation(RectangleF[] Rectangles)
         {
             animationFrames.AddRange(Rectangles);
         }
-
+        
+        /// <summary>
+        /// Makes an animation with 1 frame
+        /// </summary>
+        /// <param name="Rect">Rectangle float value</param>
         public void AddFrame(RectangleF Rect)
         {
             animationFrames.Add(Rect);
         }
+
+        /// <summary>
+        /// Makes a new animation with multiple frames
+        /// </summary>
         public void AddFrames(RectangleF[] Frames)
         {
             animationFrames.AddRange(Frames);
         }
+
+        /// <summary>
+        /// Makes an animation with multiple frames based upon values you input. 
+        /// </summary>
+        /// <param name="SpriteSize">Height/width of the frames</param>
+        /// <param name="SpriteFrames">Height/width of the texture 'grid'</param>
+        /// <param name="StartFrame">First frame of the animation</param>
+        /// <param name="StopFrame">Last frame of the animation</param>
         public void AddFrames(Point SpriteSize, Point SpriteFrames, int StartFrame, int StopFrame)
         {
             int x = 0;
@@ -43,8 +69,8 @@ namespace Shooter
             for (int i = StartFrame; i <= StopFrame; i++)
             {
                 y = 0;
-
                 x = i - 1;
+
                 while (x > SpriteFrames.X)
                 {
                     x -= SpriteFrames.X;
@@ -53,13 +79,22 @@ namespace Shooter
                 animationFrames.Add(new RectangleF(frameWidth * x, frameHeight * y, frameWidth, frameHeight));
             }
         }
+
+        /// <summary>
+        /// Clears the animationFrames
+        /// </summary>
         public void Clear()
         {
             animationFrames.Clear();
         }
+
+        /// <summary>
+        /// Selects the next frame in the animationFrames list
+        /// </summary>
         public void NextFrame()
         {
             CurrentFrame++;
+
             if (CurrentFrame >= animationFrames.Count)
             {
                 if (OnFinished != null)
@@ -67,10 +102,14 @@ namespace Shooter
                     Animating = false;
                     OnFinished(this, EventArgs.Empty);
                 }
+
                 if (Loop) CurrentFrame = 0;
             }
         }
-        float timer = 0;
+
+        /// <summary>
+        /// Keeps the animation going
+        /// </summary>
         public void Update()
         {
             if (Animating)
@@ -84,7 +123,7 @@ namespace Shooter
             }
         }
         /// <summary>
-        /// Get the cuurent frame of the animation
+        /// Get the current frame of the animation
         /// </summary>
         /// <returns></returns>
         public RectangleF GetFrame()
